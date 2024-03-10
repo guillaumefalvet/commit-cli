afa6236c05bfb5dadf8e07e59cdc366efa9ed704
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-03-10 20:17:12 +0100

    fix: console logged stderr missing

--- src/commit-cli/helpers/commit-helper.ts
+++ src/commit-cli/helpers/commit-helper.ts
@@ -7,7 +7,7 @@ import { exec } from 'child_process';
 export function commitChanges(commitMessage: string) {
   exec(`git commit -m "${commitMessage}"`, (error, stdout, stderr) => {
     if (error) {
-      console.error(`exec error: ${error}`);
+      console.error(`exec error: ${error}, stderr: ${stderr}`);
       return;
     }
     console.log(`commit: ${stdout}`);
