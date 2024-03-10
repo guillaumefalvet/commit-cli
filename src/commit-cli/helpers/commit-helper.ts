64cc32165b98ad061876aa44855ff0078e16868d
Guillaume Falvet <guillaume.falvet@outlook.com>
2024-03-10 13:16:04 +0100

    docs: added jsdoc

--- src/commit-cli/helpers/commit-helper.ts
+++ src/commit-cli/helpers/commit-helper.ts
@@ -1,5 +1,9 @@
 import { exec } from 'child_process';
 
+/**
+ * Commits the changes to the Git repository with the specified commit message.
+ * @param commitMessage - The commit message to use.
+ */
 export function commitChanges(commitMessage: string) {
   exec(`git commit -m "${commitMessage}"`, (error, stdout, stderr) => {
     if (error) {
